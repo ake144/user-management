c6826b763f176f6bf6756e39d94e5e7c6a04507c
ake144 <tamiratgedi1@gmail.com>
2025-12-28 18:44:13 +0300

    refactor(landing-page): remove commented-out LargeTestimonial section

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -31,9 +31,6 @@ export default function LandingPage() {
         <AnimatedSection className="relative z-10 max-w-[1320px] mx-auto mt-8 md:mt-16" delay={0.35}>
           <StatsSection />
         </AnimatedSection>
-        {/* <AnimatedSection className="relative z-10 max-w-[1320px] mx-auto mt-8 md:mt-16" delay={0.2}>
-          <LargeTestimonial />
-        </AnimatedSection> */}
         <AnimatedSection
           id="pricing-section"
           className="relative z-10 max-w-[1320px] mx-auto mt-8 md:mt-16"
